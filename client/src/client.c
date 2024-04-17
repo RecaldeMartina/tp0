2d2d928eebe5a03ef36379fe83c302c31fcc48d7
Martina Recalde <mrecalde@frba.utn.edu.ar>
2024-04-17 03:55:53 +0000

    logging y archivos de config

--- client/src/client.c
+++ client/src/client.c
@@ -15,7 +15,7 @@ int main(void)
 	/* ---------------- LOGGING ---------------- */
 
 	logger = iniciar_logger();
-
+	log_info(logger, "Hola! Soy un log");
 	// Usando el logger creado previamente
 	// Escribi: "Hola! Soy un log"
 
@@ -26,9 +26,11 @@ int main(void)
 
 	// Usando el config creado previamente, leemos los valores del config y los 
 	// dejamos en las variables 'ip', 'puerto' y 'valor'
-
+	ip = config_get_string_value(config, "IP");
+	puerto = config_get_string_value(config, "PUERTO");
+	valor = config_get_string_value(config, "CLAVE");
 	// Loggeamos el valor de config
-
+	log_info(logger, "VALOR leido de la config: %s", valor);
 
 	/* ---------------- LEER DE CONSOLA ---------------- */
 
@@ -54,14 +56,22 @@ int main(void)
 
 t_log* iniciar_logger(void)
 {
-	t_log* nuevo_logger;
+	t_log* nuevo_logger = log_create("tp0_log.log", "LOGGER_TP0", 1, LOG_LEVEL_INFO);
 
+	if(nuevo_logger == NULL){
+		perror("algo paso con el log. NO se pudo crear o encontrar el archivo");
+		exit(EXIT_FAILURE);
+	}
 	return nuevo_logger;
 }
 
 t_config* iniciar_config(void)
 {
-	t_config* nuevo_config;
+	t_config* nuevo_config = config_create("cliente.config");
+	if(nuevo_config == NULL){
+		perror("¡No se pudo crear el config!");
+		exit(EXIT_FAILURE);
+	}
 
 	return nuevo_config;
 }
@@ -97,4 +107,6 @@ void terminar_programa(int conexion, t_log* logger, t_config* config)
 {
 	/* Y por ultimo, hay que liberar lo que utilizamos (conexion, log y config) 
 	  con las funciones de las commons y del TP mencionadas en el enunciado */
+	log_destroy(logger);
+	config_destroy(config);
 }
